6bb048301a682177701e00cc965fbfb15f714d64
Bernhard Heinloth <heinloth@cs.fau.de>
2021-10-14 19:51:30 +0200

    Überarbeitung und Beispiele

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 WordPress Plugin `i4include`
 ===========================
 
-Dieses Plugin ermöglicht das Einbinden einer Datei.
+Dieses Plugin ermöglicht das Einbinden von HTML-Inhalten in WordPress-Seiten
+aus anderen Ressourcen (Webserver, Dateisystem).
 
-Beispiel: Durch den Shortcode in der Wordpressseite 'Test'
+
+**Beispiel:** Durch den Shortcode in der Wordpressseite 'Test'
 
     [i4include]/var/www/data/foo1.html[/i4include]
 
-wird der Inhalt der Datei `foo1.html` einfach in der Ausgabe auf
+wird der Inhalt der Datei `foo.html` einfach in der Ausgabe auf
 
     https://server/test
 
 inkludiert.
 Dabei wird immer der zum Ausgabezeitpunkt jeweils aktuelle Inhalt angezeigt,
-d.h. Änderungen an der Datei `foo1.html` sind sofort auf der Webseite ersichtlich.
+d.h. Änderungen an der Datei `foo.html` sind sofort auf der Webseite ersichtlich.
 Die erlaubten Formate zum Einbinden werden im  Array `$i4include_ext_html`
 anhand der erlaubten Dateieendung angegeben.
 
@@ -23,52 +25,87 @@ dieses Plugin jedoch noch die Möglichkeit, dynamisch Dateien aus einen ganzen
 Verzeichnisbaum auszuliefern, d.h. (relative) Verlinkungen in diesen Datei
 werden auf der Webseite unter Veibehaltung des WordPresslayouts unterstützt.
 
-Nehmen wir mal an, dass im Verzeichnis `/var/www/data/` noch die Dateien `bar.html`
-sowie  ein Unterorderner `images` mit der Bilddatei `baz.png` liegen
-und `foo2.html` sowohl einen Link `<a href="bar.html">Bar</a>` als auch eine
-Grafik `<img src="images/baz.png">` beinhaltet.
+Nehmen wir mal an, dass im Verzeichnis `/var/www/data/` neben `bar.html` auch 
+noch die Dateien `fubar.html` sowie  ein Unterorderner `images` u.a. mit der
+Bilddatei `baz.jpg` liegen und `bar.html` sowohl einen Link
+`<a href="fubar.html">Fubar</a>` als auch eine Grafik `<img src="images/baz.jpg">`
+beinhaltet.
 
-Wird nun auf der Wordpressseite 'Test' beim Einbetten der Parameter `dynamic` auf wahr gestellt
+Wird nun auf der Wordpressseite 'Test' beim Einbetten der Parameter `dynamic`
+auf wahr gestellt
 
-    [i4include dynamic="true"]/var/www/data/foo2.html[/i4include]
+    [i4include dynamic="true"]/var/www/data/bar.html[/i4include]
 
-so wird beim Öffnen der Webseiten-URL
+so wird beim Öffnen der WordPress-Webseiten-URL
 
     https://server/test
 
 automatisch auf
 
-    https://server/test/extern/foo2.html
-
-weitergeleitet. Das `extern` in der URL ist dabei ein sog. Endpoint,
-wodurch die in der URL nachfolgenden Inhalte im gleichnamigen Parameter gespeichert werden,
-also im Beispiel quasi `extern=foo2.html`.
-Durch einen Endpoint werden dynamische Inhalte auf der selben Wordpressseite ermöglicht,
-bei uns wird dieser Trick verwendet, damit nun der Link auf `bar.html` durch den Browser zu
-
     https://server/test/extern/bar.html
 
-umgewandelt wird (`extern=bar.html`), das Plugin bettet nun auf der gleichen WordPressseite
-an der Position des Shortcodes `[i4include...]` den Inhalt von `bar.html` ein --
-d.h. das WordPresslayout (Kopf und Fuß) bleibt bestehen, es sieht aus wie eine reguläre WordPressseite.
+weitergeleitet. Das `extern` in der URL ist dabei ein sog. Endpoint,
+wodurch die in der URL nachfolgenden Inhalte im gleichnamigen Parameter
+gespeichert werden, also im Beispiel quasi `extern=bar.html`.
+Durch einen Endpoint werden dynamische Inhalte auf der selben WordPress-Seite 
+ermöglicht, bei uns wird dieser Trick verwendet, damit nun der Link **Fubar**
+auf `fubar.html` durch den Browser zu
 
-Bei Binärdateien (z.B. Bilder) muss vom Plugin eine Besonderheit berücksichtigt werden:
+    https://server/test/extern/fubar.html
+
+umgewandelt wird (mit `extern=fubar.html`), das Plugin bettet nun auf der
+gleichen WordPress-Seite an der Position des Shortcodes `[i4include...]` den
+Inhalt von `fubar.html` ein -- d.h. das WordPresslayout (Kopf und Fuß) bleibt
+bestehen, es sieht aus wie eine reguläre WordPressseite.
+
+Bei Binärdateien (z.B. Bilder) muss vom Plugin eine Besonderheit berücksichtigt
+werden:
 Hier darf nicht das [HTML-]Wordpresslayout (Kopf, Menü etc) ausgegeben werden,
 sondern direkt und ausschließlich der Inhalt der Datei, zudem muss noch ein
-spezieller HTTP Header übergeben werden, welcher den Dateityp (als MIME Format) bestimmt.
+spezieller HTTP Header übergeben werden, welcher den Dateityp (als MIME Format)
+bestimmt.
 So wird zum Beispiel bei
 
-    https://server/test/extern/images/baz.png
+    https://server/test/extern/images/baz.jpg
 
-mit `extern=images/baz.png` anhand der Dateiendung erkannt, dass hier eine 
+mit `extern=images/baz.jpg` anhand der Dateiendung erkannt, dass hier eine 
 Binärdatei direkt ausgegeben werden muss.
 Die erlaubten Formate (anhand der Dateiendung) werden in `$i4include_ext_bin`
 (zusammen mit dem MIME Format) spezifiziert.
 
-Außerdem demonstriert das letzte Beispiel, dass auch Unterordner unterstützt werden.
+Außerdem demonstriert das letzte Beispiel, dass auch Unterordner unterstützt
+werden.
 
 
-Besonderheiten:
+Anwendung
+---------
+
+Der Shortcode hat den Aufbau
+
+    [i4include PARAMETER]PFAD[/i4include]
+
+wobei `PFAD` entweder ein lokaler Dateipfad (sofern dieser relativ ist, 
+wird von `$i4include_base_path` ausgegangen) oder eine Webressource (`https://...`)
+ist.
+Der Pfad muss dem in `$i4include_allowed_path` definierten regulären Ausdruck
+entsprechend.
+
+
+Die boolschen `PARAMETER` sind optional, folgende sind definiert:
+
+ * `dyanmic` erlaubt das Einbinden weiterer Dokumente (`.htm`, `.html`, ...) und
+   Binärdateien (derzeit Bilddateien `.png`, `.gif`, `.svg`, `.jpg`/`.jpeg`)
+   welche im selben Verzeichnis der angegebenen Zieldatei liegt (oder unterhalb).
+   Standardmäßig ist diese Option deaktiviert (`false`)
+ * `shortcodes` lässt WordPress Shortcodes im Zieldokument interpretieren --
+   rekursive Inkludes des selben Pfades sind jedoch nicht erlaubt.
+   (Standard: `false`)
+ * `showerrors` zeigt Fehler auf der WordPress-Webseite an (Standard: `false`)
+
+
+Voraussetzung
+-------------
+
 Dieses Plugin macht sich die Eigenschaften des bei WordPress üblichen
 Umschreiben der URLs zu nutze -- in der WordPress Administrationsoberfläche
 auch "Permalinks" genannt (unter Menüpunkt "Einstellungen").
@@ -80,4 +117,68 @@ wäre z.B. valid:
 
 
 Außerdem muss beim Shortcode immer der volle Pfad zur Datei angegeben werden,
-nur der Ordner ist (auch nicht mit der Option `dir="true"`) nicht ausreichend!
+nur der Ordner ist nicht ausreichend!
+
+
+Limitierung
+-----------
+
+ * Die Verknüpfungen in der Zieldatei müssen im `dynamic`-Modus relativ sein
+ * Es dürfen nicht mehrere `dynamic` includes mit unterschiedlichen Pfaden auf
+   der selben Seite vorkommen (aber mehrere normale includes und maximal ein
+   dynamischer sind okay).
+ * Verweise auf Ordner (ohne Dateinamen) sind nicht zulässig
+ * Einbinden von externen Stylesheets und JavaScript sollte vermieden werden,
+   denn das kann zu Problemen führen -- und zerstört das zu vereinheitlichende 
+   Look & Feel der neuen Seite.
+
+
+Verwendung / Entwicklung
+------------------------
+
+Am einfachsten mit Docker (und [Docker Compose](https://docs.docker.com/compose/install/)):
+Repo klonen, in den Ordner wechseln und
+
+    docker-compose up -d
+
+ausführen und im Webbrowser
+
+    http://localhost:8000/
+
+öffnen & die WordPress Installation durchklicken (geht schnell) und anmelden.
+
+Danach vom [RRZE-Webteam GitHub das FAU-Techfak Theme](https://github.com/RRZE-Webteam/FAU-Techfak/releases)
+als `.zip` herunter laden, und im [WordPress Administrator unter Design / Themes](http://localhost:8000/wp-admin/theme-install.php)
+auf *Theme hochladen* gehen, und die gerade eben geladene `FAU-Techfak-1.xx.zip`
+auswählen und *Jetzt installieren*.
+Danach auf der [Theme Übersichtsseite](http://localhost:8000/wp-admin/themes.php)
+noch *FAU-Techfak* aktivieren.
+
+Nun im [WordPress Administrator unter Plugins](http://localhost:8000/wp-admin/plugins.php)
+*i4include* aktivieren, zu [Einstellung / Permalinks](http://localhost:8000/wp-admin/options-permalink.php)
+wechseln und als *Individuelle Struktur* 
+
+    /%category%/%postname%
+
+angeben & *Änderungen speichern* (je nach Berechtigungen muss nun noch
+`example-wordpress/.htaccess` angepasst werden, wird aber im Interface angezeigt --
+alternativ die Schreibberechtigungen mit `chmod a+w example-wordpress/.htaccess`
+setzen und erneut speichern).
+
+Nun im [Adminmenü auf Seiten](http://localhost:8000/wp-admin/edit.php?post_type=page)
+wechseln und entweder eine neue Seite erstellen oder die *Beispiel-Seite* bearbeiten,
+dort dann einen Shortcode einfügen, z.B.
+
+    [i4include]/var/www/data/foo.html[/i4include]
+    [i4include dynamic="true"]bar.html[/i4include]
+    [i4include dynamic="true" shortcodes="true" showerrors="true"]error.html[/i4include]
+    [i4include dynamic="true"]https://www4.cs.fau.de/Lehre/WS21/V_BS/Uebungen/aufgabe0/a0.shtml[/i4include]
+
+Außerdem kann das [RRZE Elements Plugin von GitHub](https://github.com/RRZE-Webteam/rrze-elements)
+(via [*Code* / *Download ZIP*](https://github.com/RRZE-Webteam/rrze-elements/archive/refs/heads/master.zip)
+heruntergeladen und im [WordPress Admininterface](http://localhost:8000/wp-admin/plugins.php)
+installiert und aktiviert werden, damit das Beispiel
+
+    [i4include  shortcodes="true"]shortcode.html[/i4include]
+
+korrekt dargestellt wird.
--- plugin/i4include.php
+++ plugin/i4include.php
@@ -2,7 +2,7 @@
 /**
 * Plugin Name: i4include
 * Plugin URI: https://www4.cs.fau.de
-* Description: Embed HTML files (and optional their directories) in WordPress
+* Description: Einbetten von HTML Dateien (und optional das ganze Verzeichnis) für das neue i4 WordPress
 * Version: 1.0
 * Author: Bernhard Heinloth
 * Author URI: https://www4.cs.fau.de/~heinloth
@@ -16,17 +16,26 @@ $i4include_queryvar = 'extern';
 /* Der Name des Shortcodes, wie er im Editor in WordPress verwendet werden muss */
 $i4include_shortcode_name = 'i4include';
 
-/* Der Name des Shortcode-attributs, der dynamische Inhalte aktivieren kann*/
+/* Der Name des Shortcode-attributs, der dynamische Inhalte aktivieren kann */
 $i4include_shortcode_attr_dynamic = 'dynamic';
 
-/* Regulärer Ausdruck, welcher die validen Pfade für den Shortcode definiert */
+/* Der Name des Shortcode-attributs, der die Verarbeitung weiterer Shortcodes im Zieldokument erlaubt */
+$i4include_shortcode_attr_shortcode = 'shortcodes';
+
+/* Der Name des Shortcode-attributs, der die Anzeige von Fehlern auf der Webseite (zu Debugzwecken) erlaubt */
+$i4include_shortcode_attr_showerror = 'showerrors';
+
+/* Regulärer Ausdruck, welcher die validen (absoluten) Pfade für den Shortcode definiert */
 $i4include_allowed_path = '#^(http[s]?://[^/]*(?:fau|uni-erlangen)\.de/.*|/proj/i4www/.*|/var/www/data/.*)$#i';
+
+/* Basispfad für relative Pfade (sollte natürlich vom obigen regulären Ausdruck akzeptiert werden) */
+$i4include_base_path = '/var/www/data';
 
 /* Erlaubte Dateinamenerweiterungen für eingebettete Inhalte */
 $i4include_ext_html = array('htm', 'html', 'shtml', 'ushtml');
 
 /* Dateinamenerweiterungen für Binärdateien (und die dazugehörigen MIME Typen)
-  für direktes durchreichen an den Webbrowser */
+   für direktes durchreichen an den Webbrowser */
 $i4include_ext_bin = array(
 	'gif' => 'image/gif',
 	'png' => 'image/png',
@@ -35,13 +44,27 @@ $i4include_ext_bin = array(
 	'svg' => 'image/svg+xml'
 );
 
+/* Statusvariable, welche Rekursionen von includes erkennen und verhindern kann */
+$i4include_recursion = array();
+
+/* Statusvariable zum Erkennen von mehrfachen dynamic include Shortcodes auf einer Seite */
+$i4include_dynamic_path = '';
+
+/* Hilfsfunktion, welche Prüft ob es ein boolsches Shortcode-Attribut gibt
+   und ggf dessen Wert zurück gibt (falls nicht, dann wird `false` zurück gegeben */
+function i4include_attribute_as_bool($attributes, $name) {
+	return !empty($attributes) && array_key_exists($name, $attributes)
+	    && filter_var($attributes[$name], FILTER_VALIDATE_BOOLEAN);
+}
+
 
 /* Hilfsfunktion zur Bestimmung der relevanten Pfadteile.
      $shortcode_path ist der im Shortcode übergebene Pfad
      $shortcode_attr ist ein Array mit den in Shortcode angegebenen Attributen
 
    Diese Funktion liefert ein assoziatives Array mit folgenden Inhalten zurück:
-     ['base']    ist der volle Pfad des Ordners, wie er im Shortcode eingegeben wird
+     ['full']    ist der absolute Pfad der Datei die im Shortcode eingegeben wird
+     ['base']    ist das Verzeichnis zum im Shortcode angegebenen Pfad
                  (also unabhängig vom dynamischen Endpoint)
      ['link']    ist die volle URL (inklusive Endpoint) zu der aktuell angezeigten Wordpress Seite
      ['file']    ist die aktuell zu inkludierende Datei
@@ -57,23 +80,29 @@ $i4include_ext_bin = array(
                  sofern `dynamic` auf wahr gesetzt und `extern` vorhanden ist)
 */
 function i4include_pathinfo($shortcode_path, $shortcode_attr) {
-	global $i4include_queryvar, $i4include_shortcode_attr_dynamic, $i4include_allowed_path;
+	global $i4include_base_path, $i4include_dynamic_path, $i4include_queryvar, $i4include_shortcode_attr_dynamic, $i4include_allowed_path;
 	
 	// ggf. relativen Pfad anpassen 
 	if (stream_is_local($shortcode_path)) {
+		if (!path_is_absolute($shortcode_path)) {
+			$shortcode_path = $i4include_base_path.'/'.$shortcode_path;
+		}
 		$shortcode_path = realpath($shortcode_path);
 	}
 
 	// Rückgabe Array initialisieren
 	$r = array(
+		'full' => $shortcode_path,
 		'base' => dirname($shortcode_path),
 		// Prüfe, ob das Attribut `dynamic` vorhanden & auf wahr gesetzt ist
-		'dynamic' => array_key_exists($i4include_shortcode_attr_dynamic, $shortcode_attr)
-		          && filter_var($shortcode_attr[$i4include_shortcode_attr_dynamic], FILTER_VALIDATE_BOOLEAN)
+		'dynamic' => i4include_attribute_as_bool($shortcode_attr, $i4include_shortcode_attr_dynamic)
 	);
 
 	if ($r['dynamic']) {
 		// Dynamisch (Pfad anhand Shortcode sowie  `extern`, sofern vorhanden)
+		if (empty($i4include_dynamic_path)){
+			$i4include_dynamic_path = $shortcode_path;
+		}
 
 		/* Da `get_query_var()` bei nicht vorhandener Variable eine leere
 		   Zeichenkette liefert, was aber auch ein valider Wert sein kann,
@@ -146,27 +175,53 @@ add_filter('init','i4include_rewrite_endpoint');
 
 /* Das Herz: diese Funktion wird für jeden Shortcode `[i4include ...]` aufgerufen,
    liest die entsprechende Datei und gibt diese aus */
-function i4include_handler_function($atts, $content, $tag) {
-	global $i4include_ext_html, $i4include_ext_bin;
-	$pathinfo = i4include_pathinfo($content, $atts);
-	if (!$pathinfo['valid']) {
+function i4include_handler_function($attrs, $content, $tag) {
+	global $i4include_dynamic_path, $i4include_ext_html, $i4include_ext_bin, $i4include_shortcode_attr_shortcode, $i4include_shortcode_attr_showerror, $i4include_recursion;
+	$pathinfo = i4include_pathinfo($content, $attrs);
+	if ($pathinfo['dynamic'] && $i4include_dynamic_path != $pathinfo['full']) {
+		$error = 'es darf nicht mehrere <tt>dynamic includes</tt> mit unterschiedlichen Pfaden (<tt>'.$i4include_dynamic_path.'</tt> und <tt>'.$pathinfo['full'].'</tt>) auf dieser Seite geben!';
+	} else if (empty($pathinfo['path'])) {
+		$error = 'die Datei <tt>'.$pathinfo['file'].'</tt> existiert im Verzeichnis <tt>'.$pathinfo['dir'].'</tt> nicht!';
+	} else if (!$pathinfo['valid']) {
 		$error = 'das Einbetten des Pfads <tt>'.$pathinfo['path'].'</tt> ist nicht erlaubt!';
 	} else if (!in_array($pathinfo['ext'], $i4include_ext_html)) {
 		$error = 'Dateien mit der Endung <tt>'.$pathinfo['ext'].'</tt> sind nicht erlaubt!';
+	} else if (in_array($pathinfo['path'], $i4include_recursion)) {
+		$error = 'die Datei <tt>'.$pathinfo['path'].'</tt> soll erneut (endlos)rekursiv eingebunden werden!';
+	} else if ($pathinfo['dynamic'] && count($i4include_recursion) > 0) {
+		$error = 'dynamische Includes sind nur in WordPress möglich, nicht jedoch über eingebundene Seiten!';
 	} else {
-		$embed = file_get_contents($pathinfo['path']);
-		if ($embed === false) {
+		$result = file_get_contents($pathinfo['path']);
+		if ($result === false) {
 			$error = 'die Datei <tt>.'.$pathinfo['path'].'</tt> konnte nicht gelesen werden!';
 		} else {
 			// Falls von  unserer www4 Webseite was eingebettet wird, entferne Kopf und Fußzeile
-			if (preg_match('#http[s]?://www4\.[^/]+(?:fau|uni-erlangen)\.de(/.*)#i', $content, $contenturl) > 0 && preg_match('/(<div id="content">.*)<!-- beginning footer\.shtml -->/sm', $embed, $contentdiv)) {
-				$embed = str_replace(array($pathinfo['dir'], dirname($contenturl[1]).'/'), './', $contentdiv[1]);
+			// TODO: irgendwann entfernen.
+			if (preg_match('#http[s]?://www4\.[^/]+(?:fau|uni-erlangen)\.de(/.*)#i', $content, $contenturl) > 0 && preg_match('/(<div id="content">.*)<!-- beginning footer\.shtml -->/sm', $result, $contentdiv)) {
+				$result = str_replace(array($pathinfo['dir'], dirname($contenturl[1]).'/'), './', $contentdiv[1]);
 			}
-			return $embed;
+			// Sofern das Attribut `shortcodes` aktiviert ist, werden im engebundenen Dokument die Shortcodes interpretiert
+			if (i4include_attribute_as_bool($attrs, $i4include_shortcode_attr_shortcode)) {
+				// Alerdings müssen Rekursionen durch i4include verhindert werden!
+				array_push($i4include_recursion, $pathinfo['path']);
+				$result = do_shortcode($result);
+				if (($key = array_search($pathinfo['path'], $i4include_recursion)) !== false) {
+					unset($i4include_recursion[$key]);
+				}
+			}
+			// Einzubettender Inhalt
+			return $result;
 		}
 	}
-	error_log($pathinfo['link'].': '.$error);
-	return '<div style="margin:2px; padding: 2px; border:2px solid red"><b>i4include Fehler:</b> Der Inhalt kann nicht angezeigt werden &ndash; '.$error.'</div>';
+	// Fehlerbehandlung
+	if (i4include_attribute_as_bool($attrs, $i4include_shortcode_attr_showerror)) {
+		// Zeige Fehler auf der Webseite, wenn `showerrors` gesetzt ist
+		return '<div style="margin:2px; padding: 2px; border:2px solid red"><b>i4include Fehler:</b> Der Inhalt kann nicht angezeigt werden &ndash; '.$error.'<br><pre>'.print_r($pathinfo, true).'</pre></div>';
+	} else {
+		// Speichere im Log, und ignoriere Shortcode auf der Webseite
+		error_log($pathinfo['link'].': '.$error);
+		return '';
+	}
 }
 add_shortcode($i4include_shortcode_name, 'i4include_handler_function' );
 
@@ -179,7 +234,8 @@ function i4include_redirect_on_shortcode() {
 	// Untersuche Nur valide Seiten mit Inhalt
 	if (is_singular() && !empty($post->post_content)) {
 		// Prüfe, ob der i4include Shortcode verwendet wird
-		if (preg_match('/'.get_shortcode_regex(array($i4include_shortcode_name)).'/',$post->post_content, $shortcode_match) > 0) {
+		preg_match_all('/'.get_shortcode_regex(array($i4include_shortcode_name)).'/',$post->post_content, $shortcode_matches, PREG_SET_ORDER);
+		foreach ($shortcode_matches as $shortcode_match) {
 			/* $shortcode_match[3] hat nun alle Attribute und 
 			   $shortcode_match[5] den Shortcode Pfad */
 
