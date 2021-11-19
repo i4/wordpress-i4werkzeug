51884e45ad6a7344297a5aa8bd7ed9a3751a3795
Simon Schuster <simon.schuster@fau.de>
2021-11-19 15:58:02 +0000

    Add i4subnav

--- plugin/includes/i4helper.php
+++ plugin/includes/i4helper.php
@@ -22,4 +22,28 @@ function attribute_as_bool($attrs, $name) {
 	    && filter_var($attrs[$name], FILTER_VALIDATE_BOOLEAN);
 }
 
+/* stabile Sortfunktion, Quelle: Hayley Watson, https://www.php.net/manual/en/array.sorting.php
+ * Nötig, da PHP erst ab PHP8 eine stabile Sortierfunktion in der Standardlib anbietet
+ */
+function stable_usort(&$array, $cmp)
+{
+    $i = 0;
+    $array = array_map(function($elt)use(&$i)
+    {
+        return [$i++, $elt];
+	}, $array);
+    usort($array, function($a, $b)use($cmp)
+    {
+        return $cmp($a[1], $b[1]) ?: ($a[0] - $b[0]);
+    });
+    $array = array_column($array, 1);
+}
+
+/* Hilfsfunktion welche beliebigen Text in eine als `id`-Feld eines HTML-Tags geeignete
+ * Darstellung überführt
+ */
+function to_anchortext($text) {
+	return preg_replace('/[^a-z0-9]+/', '_', str_replace(array('ä', 'ö', 'ü'), array('ae', 'oe', 'ue'), strtolower($text)));
+}
+
 ?>
